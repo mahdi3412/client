dcf2d06360d10613596a4759be377de925bb9f62
mahdi3412 <mahdimem65@gmail.com>
2022-11-09 14:48:25 +0330

    14010818

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import cam from '../Image/cam.png'
 import more from '../Image/more.png'
 import add from '../Image/add.png'
+import Messages from './Messages'
+import Input from './Input'
 
  const Chat = () => {
   return (
-    <div className='chat'>Chat
-    <img src={cam} alt=""/>
-    <img src={more} alt=""/>
-    <img src={add} alt=""/>
+    <div className='chat'>
+        <div className="chatInfo">
+           <span>Mahdi</span>
+                <div className="chatIcons">
+                    <img src={cam} alt=""/>
+                    <img src={more} alt=""/>
+                    <img src={add} alt=""/>
+                </div>
+         </div>
+         <Messages/>
+         <Input/>
     </div>
   )
 }
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import img from "../Image/img.png"
+import attach from "../Image/attach.png"
 export const Input = () => {
   return (
-    <div>Input</div>
+   <div className="input">
+        <input type="text" placeholder='Type something...' />
+      <div className="send">
+         <img src={attach} alt=''/>
+         <input type="file" style={{display:"none"}} id="file" />
+          <label htmlFor='file'>
+          <img src={img} alt=''/>
+          </label>
+          <button>Send</button>
+        </div>
+   </div>
   )
 }
+export default Input
